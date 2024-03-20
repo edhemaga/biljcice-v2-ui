2409f0c470271d28d9e569d881902c7f88211b68
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2024-03-20 18:57:21 +0100

    Env var

--- src/shared/traffic/axios.ts
+++ src/shared/traffic/axios.ts
@@ -4,7 +4,7 @@ import axios, {
 } from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: process.env.BE || "http://localhost:3001",
     params: {}
     //baseURL: process.env.HOST
 })
